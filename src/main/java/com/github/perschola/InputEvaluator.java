ad15c75d97973e5df6ab87cd8e1bb97c9f8945a4
Erjon Dervishaj <er_djs@yahoo.com>
2019-12-02 16:02:38 -0500

    first commit

--- src/main/java/com/github/perschola/InputEvaluator.java
+++ src/main/java/com/github/perschola/InputEvaluator.java
@@ -1,5 +1,8 @@
 package com.github.perschola;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputEvaluator {
     public void run() {
         // prompt user to input number
@@ -12,5 +15,21 @@ public class InputEvaluator {
             // increment counter by 1
 
         // display product to user
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter an integer");
+        do{
+            int input = scanner.nextInt();
+
+
+
+        int product = 1;
+
+        for (int i = 1; i <= input; i++)
+            product *= i;
+
+        System.out.println("The factorial of " + input + " " + product);
+
+
     }
 }
